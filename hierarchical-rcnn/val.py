8359e787782a4e30069d9488f4993a25e9b5b43c
Henry Lydecker <henry.lydecker@gmail.com>
2023-02-09 13:51:05 +1100

    Update val.py

--- hierarchical-rcnn/val.py
+++ hierarchical-rcnn/val.py
@@ -1,17 +1,38 @@
 import argparse
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 def validate_hierarchical_mask_rcnn(model, val_loader, loss_fn):
     model.eval()
     total_loss = 0
+    true_labels = []
+    pred_labels = []
     with torch.no_grad():
         for images, gt_classes, gt_bboxes, gt_masks in val_loader:
             class_logits, class_probs, bboxes, mask_logits, masks = model(images)
+            _, pred_classes = torch.max(class_probs, dim=1)
+            true_labels.append(gt_classes.detach().cpu().numpy())
+            pred_labels.append(pred_classes.detach().cpu().numpy())
             loss = loss_fn(class_logits, class_probs, gt_classes, bboxes, mask_logits, gt_masks)
             total_loss += loss.item()
 
+    true_labels = np.concatenate(true_labels)
+    pred_labels = np.concatenate(pred_labels)
+    confusion_matrix = confusion_matrix(true_labels, pred_labels)
     avg_loss = total_loss / len(val_loader)
     print(f"Validation Loss: {avg_loss}")
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(confusion_matrix, cmap="Blues")
+    ax.set_xticks(np.arange(confusion_matrix.shape[1]))
+    ax.set_yticks(np.arange(confusion_matrix.shape[0]))
+    ax.set_xlabel("Predicted Label")
+    ax.set_ylabel("True Label")
+    plt.colorbar(im, ax=ax)
+    plt.tight_layout()
+    plt.savefig("confusion_matrix.png")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Validate a HierarchicalMaskRCNN model")
